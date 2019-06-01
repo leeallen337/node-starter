c912b3ba16fee372d6c085cbc73e32b2e34e619b
Lee Allen <leeallen337@gmail.com>
2019-06-01 14:38:09 -0400

    Add BaseModel

--- models/BaseModel.js
+++ models/BaseModel.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const objection = require('objection');
+const { Model } = require('objection');
+
+class BaseModel extends Model {
+  $beforeInsert() {
+    if (this.createdAt || this.updatedAt) {
+      throw new objection.ValidationError({
+        message: 'createdAt and updatedAt are invalid fields',
+        type: 'DateError',
+        data: {}
+      });
+    }
+  }
+
+  $beforeUpdate() {
+    if (this.createdAt || this.updatedAt) {
+      throw new objection.ValidationError({
+        message: 'createdAt and updatedAt are invalid fields',
+        type: 'DateError',
+        data: {}
+      });
+    }
+  }
+}
+
+module.exports = BaseModel;
