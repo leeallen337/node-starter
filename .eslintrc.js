2e1c7d373f14df1c169cce92dfc6db0cbe0c47c2
Lee Allen <leeallen337@gmail.com>
2020-03-24 20:16:34 -0400

    Rename config files

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2020: true,
+    mocha: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
+  ignorePatterns: ["dist/"],
+};
